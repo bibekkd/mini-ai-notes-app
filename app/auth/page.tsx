610f8766667119d6fe7f5a1e225af615b52717ee
Bibek Dey <bibek@Bibeks-MacBook-Air.local>
2025-04-22 01:55:18 +0530

    nextjs setup, shadcn setup completed and auth page ongoing

---
+++ app/auth/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex items-center justify-center w-full h-screen'>
+        <div className='w-96 h-96 border rounded-md p-5'>
+
+        </div>
+    </div>
+  )
+}
+
+export default page
